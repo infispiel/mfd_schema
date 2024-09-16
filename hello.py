ffc4ca61d1aae04fba0f12d83912d54aad64d6c4
infispiel <infinite.loopholes@gmail.com>
2024-09-16 12:12:08 -0400

    init commit

---
+++ hello.py
@@ -0,0 +1,24 @@
+from lark import Lark
+
+def main():
+    schema_parser = Lark(open("grammar.lark"), start="value", 
+                         maybe_placeholders=True,
+                         propagate_positions=True)
+
+    ex_1 = ""
+    with open('examples/version_ex') as file :
+        ex_1 = file.read()
+
+    t = schema_parser.parse(ex_1)
+    print( t.pretty())
+
+    ex_2 = ""
+    with open('examples/contributor_ex') as file :
+        ex_2 = file.read()
+
+    t = schema_parser.parse(ex_2)
+    print( t.pretty() )
+
+
+if __name__ == "__main__":
+    main()
